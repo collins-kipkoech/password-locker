fa8ba633dd1a5f79a689cfbe79a6f2160fce5c82
kipkoech-msojo <collinskipkoech95@gmail.com>
2020-10-10 22:35:56 +0300

    created the init method to initialize the objects attributes

---
+++ passlock.py
@@ -0,0 +1,10 @@
+class User:
+    """
+    class to create new user
+    """
+    def __init__(self,username,password):
+        self.username = username
+        self.password = password
+        """
+        defined an init method to initialize the user attributes
+        """
