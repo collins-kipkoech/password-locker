e4c867ccd1b047f6edc80391278d3e35cb6ef1bb
kipkoech-msojo <collinskipkoech95@gmail.com>
2020-10-12 07:30:39 +0300

    created save and delete credentials methods for adding and removing user credentials from the credentials list

--- passlock.py
+++ passlock.py
@@ -30,4 +30,34 @@ class User:
     def display_user_details(cls):
         return cls.user_list
 
+
+class Credentials:
+    """
+    created credentials class
+    """
+
+    credentials_list = []
+
+    def __init__(self,account,username,passowrd):
+        self.account = account
+        self.username = username
+        self.password = passoword
+        """
+        method to initialize user credential details
+        """
+
+    def save_credentials(self):
+        """
+        method to save user credentials to the credentials list
+        """
+        Credentials.credentials_list.append(self)
+
+    def delete_credentials(self):
+        """
+        method to delete credentials from the credentials list
+        """
+        Credentials.credentials_list.remove(self)
+
+    
+
     
